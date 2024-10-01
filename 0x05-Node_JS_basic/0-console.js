f61d9888fa8bab3cd7b7d2743f77d436d3f06f03
“BillyWaks” <136112711+BillyWaks@users.noreply.github.com>
2024-10-01 16:13:36 +0300

    updated_file

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,19 @@
-// function to display message
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
 
+// function to display message
 function displayMessage(message) {
     console.log(message);
 }
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
 
 // Export the function to make it available for other modules
 module.exports = displayMessage;
